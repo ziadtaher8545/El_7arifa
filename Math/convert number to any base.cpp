36ca981a85127b49f28ec44c941dd8311decd1c4
ibrahim-ashraf597 <ia9319138@gmail.com>
2026-08-02 14:08:39 +0300

    Create convert number to any base.cpp

---
+++ Math/convert number to any base.cpp
@@ -0,0 +1,13 @@
+﻿string toBase(long long num, int base) {
+    if (num == 0) return "0";
+
+    const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string result = "";
+
+    while (num > 0) {
+        result = digits[num % base] + result;
+        num /= base;
+    }
+
+    return result;
+}
