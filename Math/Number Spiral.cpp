41e01a9ff6b6d0e5f3dc8aed26d56a4b84826d07
ibrahim-ashraf597 <ia9319138@gmail.com>
2026-08-02 14:04:34 +0300

    Create Number Spiral.cpp

---
+++ Math/Number Spiral.cpp
@@ -0,0 +1,21 @@
+﻿ll y, x;
+cin >> y >> x;
+ll n = max(y, x);
+ll result;
+if (n % 2 == 0) {
+    if (y == n) {
+        result = n * n - x + 1;
+    }
+    else {
+        result = (n - 1) * (n - 1) + y;
+    }
+}
+else {
+    if (x == n) {
+        result = n * n - y + 1;
+    }
+    else {
+        result = (n - 1) * (n - 1) + x;
+    }
+}
+cout << result << '\n';
