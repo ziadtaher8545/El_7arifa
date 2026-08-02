eb3729f0d93c83129c10388e2008305b77cdae54
ibrahim-ashraf597 <ia9319138@gmail.com>
2026-08-02 14:05:42 +0300

    Create Prime Factorization.cpp

---
+++ Math/Prime Factorization.cpp
@@ -0,0 +1,22 @@
+﻿
+vector<int> prime_factors(int n) {
+    vector<int> factors;
+
+    while (n % 2 == 0) {
+        factors.push_back(2);
+        n /= 2;
+    }
+
+    for (int i = 3; i * i <= n; i += 2) {
+        while (n % i == 0) {
+            factors.push_back(i);
+            n /= i;
+        }
+    }
+
+    if (n > 2) {
+        factors.push_back(n);
+    }
+
+    return factors;
+}
