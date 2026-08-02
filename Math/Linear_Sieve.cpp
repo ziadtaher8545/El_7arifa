16bfd37b7a384c711c21f616319eb4ab5d4ef31d
ibrahim-ashraf597 <ia9319138@gmail.com>
2026-08-02 14:00:56 +0300

    Create Linear_Sieve.cpp

---
+++ Math/Linear_Sieve.cpp
@@ -0,0 +1,18 @@
+const int N = 1e8;
+vector<int> lp(N + 1);
+vector<int> pr;
+
+void Linear_Sieve() {
+    for (int i=2; i <= N; ++i) {
+        if (lp[i] == 0) {
+            lp[i] = i;
+            pr.push_back(i);
+        }
+        for (int j = 0; i * pr[j] <= N; ++j) {
+            lp[i * pr[j]] = pr[j];
+            if (pr[j] == lp[i]) {
+                break;
+            }
+        }
+    }
+}
