eac6110c1544d7fd432f79b6616caffcc054a56b
ibrahim-ashraf597 <ia9319138@gmail.com>
2026-08-02 14:24:15 +0300

    Implement segmented sieve algorithm for prime detection

---
+++ Math/segmentedSieve best memory .cpp
@@ -0,0 +1,11 @@
+vector<bool> segmentedSieveNoPreGen(long long L, long long R) {
+    // time  is ((R-L+1)*log (R))+sqrt(R))
+    vector<bool> isPrime(R - L + 1, true);
+    long long lim = sqrt(R);
+    for (long long i = 2; i <= lim; ++i)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
