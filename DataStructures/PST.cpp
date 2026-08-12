4c28d02aa67a8b266b10d3de8947dd54b83a95a7
Ziad taher <ziadtaherb@gmail.com>
2026-08-12 20:35:22 +0300

    Update PST.cpp

--- DataStructures/PST.cpp
+++ DataStructures/PST.cpp
@@ -1,116 +1,98 @@
 struct Node
 {
-    Node* l, * r;
-    ll sum = 0, lazy = 0;
+    Node *l, *r;
+    ll sum;
 
-    Node(int val) : l(NULL), r(NULL), sum(val) {}
-    Node() : l(NULL), r(NULL) {}
-    Node(Node* l, Node* r) : l(l), r(r) {
-        if (l != NULL)
+    Node(ll val = 0) : l(nullptr), r(nullptr), sum(val) {}
+    Node(Node *l, Node *r) : l(l), r(r), sum(0)
+    {
+        if (l)
             sum += l->sum;
-        if (r != NULL)
+        if (r)
             sum += r->sum;
     }
-
-    void apply(int s, int e, ll val) {
-        sum += val * (e - s + 1);
-        lazy += val;
-    }
-
-    void push(int s, int e) {
-        if (lazy == 0)
-            return;
-        int mid = s + e >> 1;
-
-        l = l == NULL ? new Node() : new Node(*l);
-        l->apply(s, mid, lazy);
-
-        r = r == NULL ? new Node() : new Node(*r);
-        r->apply(mid + 1, e, lazy);
-
-        lazy = 0;
-    }
 };
+
 class PST
 {
 private:
     int n;
-    Node merge(Node lf, Node ri) {
-        Node ret;
-        ret.sum = lf.sum + ri.sum;
-        return ret;
-    }
 
-    // range update
-    Node* update(Node* node, int s, int e, int l, int r, ll val) {
-        if (r < s || e < l || !node)
-            return node;
-        if (l <= s && e <= r) {
-            Node* cur = new Node(*node);
-            cur->apply(s, e, val);
-            return cur;
-        }
+    ll getSum(Node *node) { return node ? node->sum : 0; }
+    Node *getL(Node *node) { return node ? node->l : nullptr; }
+    Node *getR(Node *node) { return node ? node->r : nullptr; }
 
-        node->push(s, e);
-        int mid = s + e >> 1;
-        return new Node(update(node->l, s, mid, l, r, val), update(node->r, mid + 1, e, l, r, val));
-    }
-
-    // point update
-    Node* update(Node* node, int s, int e, int i, ll val) {
+    Node *update(Node *node, int s, int e, int idx, ll val)
+    {
         if (s == e)
             return new Node(val);
-        int mid = s + e >> 1;
-        if (i <= mid)
-            return new Node(update(node->l, s, mid, i, val), node->r);
+
+        int mid = s + (e - s) / 2;
+        Node *cur = new Node();
+        if (node)
+        {
+            cur->l = node->l;
+            cur->r = node->r;
+        }
+
+        if (idx <= mid)
+            cur->l = update(getL(cur), s, mid, idx, val);
         else
-            return new Node(node->l, update(node->r, mid + 1, e, i, val));
+            cur->r = update(getR(cur), mid + 1, e, idx, val);
+
+        cur->sum = getSum(cur->l) + getSum(cur->r);
+        return cur;
     }
-    Node query(Node* node, int s, int e, int l, int r) {
+
+    ll query(Node *node, int s, int e, int l, int r)
+    {
         if (!node || r < s || e < l)
-            return Node();
+            return 0;
         if (l <= s && e <= r)
-            return *node;
+            return node->sum;
 
-        node->push(s, e);
-        int m = (s + e) / 2;
-        return merge(query(node->l, s, m, l, r), query(node->r, m + 1, e, l, r));
+        int mid = s + (e - s) / 2;
+        return query(node->l, s, mid, l, r) + query(node->r, mid + 1, e, l, r);
     }
 
-    int getKth(Node* cur, Node* prv, int k, int s, int e) {
-        if (e == s)
+    int getKth(Node *cur, Node *prv, int s, int e, int k)
+    {
+        if (s == e)
             return s;
 
-        int mid = s + e >> 1;
-        ll res = cur->l->sum - prv->l->sum;
-        if (res >= k)
-            return getKth(cur->l, prv->l, k, s, mid);
+        int mid = s + (e - s) / 2;
+
+        ll left_sum = getSum(getL(cur)) - getSum(getL(prv));
+
+        if (left_sum >= k)
+            return getKth(getL(cur), getL(prv), s, mid, k);
         else
-            return getKth(cur->r, prv->r, k - res, mid + 1, e);
+            return getKth(getR(cur), getR(prv), mid + 1, e, k - left_sum);
     }
 
 public:
     PST(int n) : n(n) {}
-    Node* build(int s, int e, const vector<int>& a) {
+
+    Node *build(int s, int e, const vector<int> &a)
+    {
         if (s == e)
             return new Node(a[s]);
-        int m = (s + e) / 2;
-        return new Node(build(s, m, a), build(m + 1, e, a));
+        int mid = s + (e - s) / 2;
+        return new Node(build(s, mid, a), build(mid + 1, e, a));
     }
 
-    // range update
-    Node* update(Node* node, int l, int r, int val) {
-        return update(node, 0, n - 1, l, r, val);
-    }
-    // point update
-    Node* update(Node* node, int i, ll val) {
-        return update(node, 0, n - 1, i, val);
+    Node *update(Node *node, int idx, ll val)
+    {
+        return update(node, 0, n - 1, idx, val);
     }
 
-    ll query(Node* node, int l, int r) {
-        return query(node, 0, n - 1, l, r).sum;
+    ll query(Node *node, int l, int r)
+    {
+        return query(node, 0, n - 1, l, r);
     }
-    ll getKth(Node* cur, Node* prv, int k) {
-        return getKth(cur, prv, k, 0, n - 1);
+
+    int getKth(Node *cur, Node *prv, int k)
+    {
+        return getKth(cur, prv, 0, n - 1, k);
     }
 };
