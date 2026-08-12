33cd7e400ec6a1ba4abe03a730b0fff84662d5fe
Ziad taher <ziadtaherb@gmail.com>
2026-08-12 16:01:37 +0300

    Add function to get next greater element using stack
    Implement a function to find the next greater element for each element in the array using a monotonic stack.

---
+++ تيموسيات /Monotonic Stack Next Greater Element.cpp
@@ -0,0 +1,17 @@
+vector<int>
+get_next_greater_element(vector<int>& a) {
+    int n = a.size();
+    vector<int> nge(n, -1);
+    stack<int> st;
+    for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && a[st.top()] <=
+       a[i]) {
+            st.pop();
+        }
+        if (!st.empty()) {
+            nge[i] = st.top();
+        }
+        st.push(i);
+    }
+    return nge;
+}
