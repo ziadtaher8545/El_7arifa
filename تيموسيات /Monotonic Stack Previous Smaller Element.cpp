bd5f3861655ef976793d63bcdac7757958cc15ec
Ziad taher <ziadtaherb@gmail.com>
2026-08-12 16:04:50 +0300

    Create Monotonic Stack Previous Smaller Element.cpp

---
+++ تيموسيات /Monotonic Stack Previous Smaller Element.cpp
@@ -0,0 +1,17 @@
+vector<int>
+get_previous_smaller_element(vector<int>& a) {
+    int n = a.size();
+    vector<int> nge(n, -1);
+    stack<int> st;
+    for (int i = 0; i < n; ++i) {
+        while (!st.empty() && a[st.top()] >=
+       a[i]) {
+            st.pop();
+        }
+        if (!st.empty()) {
+            nge[i] = st.top();
+        }
+        st.push(i);
+    }
+    return nge;
+}
