7a296d032343ae59804b2ce29e9307048867c92e
Ziad taher <ziadtaherb@gmail.com>
2026-08-12 16:06:24 +0300

    Create  Monotonic Stack Previous Equal (Strictly Smaller In Between).cpp

---
+++ تيموسيات /Monotonic Stack Previous Equal (Strictly Smaller In Between).cpp
@@ -0,0 +1,18 @@
+vector<int>
+get_prev_equal_smaller_between(vector<int>& a) {
+    int n = a.size();
+    vector<int> eq(n, -1);
+    stack<int> st;
+    for (int i = 0; i < n; ++i) {
+        while (!st.empty() && a[st.top()] <
+       a[i]) {
+            st.pop();
+        }
+        if (!st.empty() && a[st.top()] ==
+       a[i]) {
+            eq[i] = st.top();
+        }
+        st.push(i);
+    }
+    return eq;
+}
