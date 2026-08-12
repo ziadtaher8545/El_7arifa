99394e4df3a2b54b665d5ea7b3970dc631ea3ce4
Ziad taher <ziadtaherb@gmail.com>
2026-08-12 16:05:11 +0300

    Create Monotonic Stack Previous Greater Element.cpp

---
+++ تيموسيات /Monotonic Stack Previous Greater Element.cpp
@@ -0,0 +1,18 @@
+vector<int>
+get_previous_greater_element(vector<int>
+& a) {
+    int n = a.size();
+    vector<int> nge(n, -1);
+    stack<int> st;
+    for (int i = 0; i < n; ++i) {
+        while (!st.empty() && a[st.top()] <=
+       a[i]) {
+            st.pop();
+        }
+        if (!st.empty()) {
+            nge[i] = st.top();
+        }
+        st.push(i);
+    }
+    return nge;
+}
