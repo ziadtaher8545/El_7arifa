c156027a2f44eb18eb48e29a07d76d1211bef170
Ziad taher <ziadtaherb@gmail.com>
2026-08-16 00:01:30 +0300

    Update OrderedSet.cpp

--- DataStructures/OrderedSet.cpp
+++ DataStructures/OrderedSet.cpp
@@ -1,8 +1,9 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> Ordered_Set;
-//! find_by_order : element at index i
-//! order_of_key  : how many elements < A
-//! greater<int>
-//! less_equal<int>, greater_equal<int> : multiset
+template<class T>
+using Orderd_Set = tree < T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// ! find_by_order : element at index i
+// ! order_of_key  : how many elements < A
+// ! greater<int>
+// ! less_equal<int>, greater_equal<int> : multiset
